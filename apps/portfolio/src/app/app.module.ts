cc8aa14c7bb318ae23607c103221757995499532
Drew Thompson <drew@commongenius.com>
2019-09-11 12:32:25 -0700

    refactor(*): create and provide environment in app module

--- apps/portfolio/src/app/app.module.ts
+++ apps/portfolio/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpLoaderFactory } from '@portfolio/core/config';
+import { ENVIRONMENT, HttpLoaderFactory } from '@portfolio/core/config';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -20,7 +21,7 @@ import { AppComponent } from './app.component';
       }
     })
   ],
-  providers: [],
+  providers: [{ provide: ENVIRONMENT, useValue: environment }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- libs/core/config/src/lib/core-config.ts
+++ libs/core/config/src/lib/core-config.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { InjectionToken } from '@angular/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 /**
@@ -7,3 +8,14 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
+
+/**
+ * Injection token for an app's environment file.
+ * This **must be provided** in each app's `app.module.ts`.
+ *
+ * @example
+ * providers: [
+ *  { provide: ENVIRONMENT, useValue: environment }
+ * ]
+ */
+export const ENVIRONMENT = new InjectionToken<string>('environment');
