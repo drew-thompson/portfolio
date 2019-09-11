082d75fabe6057f72f3946cc257c66aeb69ca6f1
Drew Thompson <drew@commongenius.com>
2019-09-11 12:50:00 -0700

    refactor(libs): initialize shared library

---
+++ libs/shared/src/lib/shared.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { CommonMaterialModule } from '@portfolio/common/material';
+
+@NgModule({
+  exports: [CommonMaterialModule]
+})
+export class SharedModule {}
