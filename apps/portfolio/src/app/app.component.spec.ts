6200b11b6d6ba64027f34985f617d6b2db4f1baf
Drew Thompson <drew@commongenius.com>
2019-09-11 11:32:42 -0700

    fix(app): correct app component unit test

--- apps/portfolio/src/app/app.component.spec.ts
+++ apps/portfolio/src/app/app.component.spec.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService
+} from '@ngx-translate/core';
+import { HttpLoaderFactory } from '@portfolio/core/config';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientModule]
+      imports: [
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [TranslateService]
     }).compileComponents();
   }));
 
