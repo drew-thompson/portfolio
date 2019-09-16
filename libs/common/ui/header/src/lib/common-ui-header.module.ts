a5e638635f93588cb828499fc4d00cebc7b31a70
Drew Thompson <drew@commongenius.com>
2019-09-16 09:49:34 -0700

    refactor(*): integrate flex layout and add basic responsive nav design

--- libs/common/ui/header/src/lib/common-ui-header.module.ts
+++ libs/common/ui/header/src/lib/common-ui-header.module.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatIconModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
 })
