617c7185aa200391c6bd42afd311addfb610db9b
Drew Thompson <drew@commongenius.com>
2019-09-21 10:19:17 -0700

    refactor(*): combine Lato font with Roboto and style material typography

--- libs/home/ui/src/lib/home-ui.module.spec.ts
+++ libs/home/ui/src/lib/home-ui.module.spec.ts
@@ -1,11 +1,12 @@
 import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CommonMaterialModule } from '@portfolio/common/material';
 import { HomeUiModule } from './home-ui.module';
 
 describe('HomeUiModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HomeUiModule, CommonMaterialModule]
+      imports: [HomeUiModule, CommonMaterialModule, RouterTestingModule]
     }).compileComponents();
   }));
 
--- libs/home/ui/src/lib/home-ui.module.ts
+++ libs/home/ui/src/lib/home-ui.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonMaterialModule } from '@portfolio/common/material';
 import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
-  imports: [CommonModule, CommonMaterialModule],
+  imports: [CommonModule, CommonMaterialModule, RouterModule],
   declarations: [TopicComponent],
   exports: [TopicComponent]
 })
