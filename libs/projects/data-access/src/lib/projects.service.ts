334f40fb4db07ee809862c704820bbd14484897e
Drew Thompson <drew@commongenius.com>
2019-09-27 23:29:21 -0700

    refactor(project): resolve project and display raw data on detail page

--- libs/projects/data-access/src/lib/projects.service.ts
+++ libs/projects/data-access/src/lib/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Project } from '@portfolio/data/models';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,18 @@ import { Observable } from 'rxjs';
 export class ProjectsService {
   constructor(private db: AngularFirestore) {}
 
+  /**
+   * Return all projects stored in the Firestore, ordered by index.
+   */
   getProjects(): Observable<Project[]> {
-    return this.db
-      .collection<Project>('projects', ref => ref.orderBy('index', 'asc'))
-      .valueChanges();
+    return this.db.collection<Project>('projects', ref => ref.orderBy('index', 'asc')).valueChanges();
+  }
+
+  /**
+   * Get a specific project with a given uid. This instance will react directly to changes made in the database.
+   * @param url Url of the project, which acts as a uid
+   */
+  getProjectValueChanges(url: string): Observable<Project> {
+    return this.getProjects().pipe(map(projects => projects.find(p => p.url === url)));
   }
 }
--- libs/projects/utils/src/lib/project.resolver.ts
+++ libs/projects/utils/src/lib/project.resolver.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Project } from '@portfolio/data/models';
-import { Observable, of } from 'rxjs';
+import { ProjectsService } from '@portfolio/projects/data-access';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectResolver implements Resolve<Project> {
-  resolve(): Observable<Project> {
-    return of({ title: 'hi' } as Project);
+  constructor(private projectsService: ProjectsService) {}
+
+  resolve(next: ActivatedRouteSnapshot): Observable<Project> {
+    const id = next.paramMap.get('id');
+
+    return this.projectsService.getProjectValueChanges(id).pipe(
+      take(1) // Must take first emitted value when resolving
+    );
   }
 }
